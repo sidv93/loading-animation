ef118a8498a8993613b1a842b5fb92ddc26c9567
Siddharth Venkatesh <siddhuv93@gmail.com>
2019-10-15 01:09:37 +0530

    initial load animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from './global.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'loading-animation';
+
+  public constructor(private globalService: GlobalService) {}
+
+  public ngOnInit() {
+    this.globalService.loading = true;
+    setTimeout(() => {
+      this.globalService.loading = false;
+    }, 4000);
+  }
 }
